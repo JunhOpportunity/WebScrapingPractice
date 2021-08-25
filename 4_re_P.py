279e39b03633b1bbbc6936667ff8f02151d1f261
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2021-08-25 23:17:20 +0900

    정규식 기본 1 practice

--- 4_re_P.py
+++ 4_re_P.py
@@ -1,1 +1,16 @@
-i
+import re
+
+p = re.compile("ca.e")
+# . : 하나의 문자를 의미    ex) ("ca.e") = care, cafe, case, ...
+# ^ : 문자열의 시작         ex) ("^de") = desk, destination, ...
+# $ : 문자열의 끝           ex) ("se$") = case, base, ...
+
+m = p.match("careless")
+
+def print_match(m):
+    if m:
+        print("매칭")
+    else:
+        print("매칭되지 않음")
+
+print_match(m)
