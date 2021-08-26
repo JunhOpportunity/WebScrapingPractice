7082a776f5a06b03deb6611949bde3f99bcf94f1
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2021-08-26 21:29:54 +0900

    USER AGENT TEST

---
+++ 5_user_agent_P.py
@@ -0,0 +1,6 @@
+import requests
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"}
+res = requests.get("http://naver.com", headers=headers)
+res.raise_for_status()
+with open("NewNaverFile.html", "w", encoding="utf8") as f:
+    f.write(res.text)
