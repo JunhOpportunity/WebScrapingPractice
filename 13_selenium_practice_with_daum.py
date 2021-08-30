eb418ba4bd268951cdcc787b0a616b785589b5ee
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2021-08-30 21:43:04 +0900

    selenium with daum search and click

---
+++ 13_selenium_practice_with_daum.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+browser = webdriver.Chrome()
+browser.get("https://www.daum.net")
+
+elem = browser.find_element_by_id("q")
+elem
+
+from selenium.webdriver.common.keys import Keys
+elem.send_keys("네이버웹툰")
+elem = browser.find_element_by_xpath("//*[@id='daumSearch']/fieldset/div/div/button[2]")    # 돋보기 버튼의 xpath 복사 후 붙여넣기
+elem.click()    # 돋보기 버튼은 클릭해야 작동하므로 .click 함
