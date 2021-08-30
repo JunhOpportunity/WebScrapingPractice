b8c6d7697e9ad2dc3c2149141515dd7cd20f7ea6
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2021-08-30 21:29:28 +0900

    selenium with naver login and search

---
+++ 13_selenium_practice_wtih_naver.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+
+browser = webdriver.Chrome()        # chromedriver.exe가 현재 폴더에 있다면 괄호 비워도 가능 / chromedriver.exe가 현재 폴더에 없다면 경로 적어주어야 함.
+
+# 네이버에서 활용을 통한 기본 개념 공부
+browser.get("http://naver.com")
+# elem = browser.find_element_by_class_name("list_login")   # 원래 이 코드였는데, 이 코드대로 한 후 바로 from selenium.webdriver.common.keys import Keys 후 elem.send_keys("") 하면 당연히 오류뜬다.
+elem = browser.find_element_by_id("")  # 이걸로 elem을 바꿔주어야 가능하다.
+elem.click()        # 버튼 클릭
+from selenium.webdriver.common.keys import Keys
+elem.send_keys("네이버 검색")           # 검색창에 원하는 내용 입력
+elem.send_keys(Keys.ENTER)             # 검색!
+
+elem = browser.find_element_by_tag_name("a")    # a 태그를 가진 첫 번째 엘리먼트만 가져옴
+elem
+
+elem = browser.find_elements_by_tag_name("a")   # a 태그를 가진 모든 엘리먼트를 가져옴 (elements)
+elem
+
+for e in elem:
+    e.get_attribute("href")     # 태그 빼고 텍스트만 가져오기
+
--- 3_Request_P.py
+++ 3_Request_P.py
@@ -2,3 +2,4 @@ import requests
 res = requests.get("http://naver.com")
 res.raise_for_status()
 print("응답코드 :", res.status_code)    # 응답코드 200이면 정상
+
