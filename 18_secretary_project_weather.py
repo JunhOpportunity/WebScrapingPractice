8afd82c2b3f44119a6ae37b1a39709f255ac7d82
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2021-09-01 21:36:26 +0900

    dust modify and did'n understand part check

--- 18_secretary_project_weather.py
+++ 18_secretary_project_weather.py
@@ -13,11 +13,13 @@ soup = BeautifulSoup(res.text, "lxml")
 weather_inform = soup.find("p",attrs={"class":"cast_txt"}).get_text()   # 날씨 요약
 temperature = soup.find("span", attrs={"class":"todaytemp"}).get_text()     # 현재 온도
 sensible_temperature = soup.find("span", attrs={"class":"sensible"}).find("span", attrs={"class":"num"}).get_text()     # 체감 온도
-fine_dust = soup.find("dd", attrs={"class":"lv1"}).get_text()   # 미세먼지 농도와 상태
-
+# ★★★★★★★★★여기 아랫부분 인덱스로 처리하는거 이해 안감.★★★★★★★★★★★★★★★
+fine_dust = soup.find("dl", attrs={"class":"indicator"})   # 미세먼지 농도와 상태
+dust_pm10 = fine_dust.find_all("dd")[0].get_text()
+dust_pm25 = fine_dust.find_all("dd")[1].get_text()
 print("-" * 30)
 print("오늘의 날씨")
 print(weather_inform)
 print(f"현재 온도는 {temperature}º 이고, 체감 온도는 {sensible_temperature}º입니다")
-print(f"마지막으로 미세먼지 농도는 {fine_dust}입니다")
+print(f"마지막으로 미세먼지 농도는 {dust_pm10}, 초미세먼지 농도는 {dust_pm25}입니다")
 print("-" * 30)
